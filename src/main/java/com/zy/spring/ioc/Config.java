44fb6f9c65d6b01cc70c969ee4e8164da6202d29
zy <1661996964@qq.com>
2022-11-07 22:47:08 +0800

    Java Config对象依赖注入

--- src/main/java/com/zy/spring/ioc/Config.java
+++ src/main/java/com/zy/spring/ioc/Config.java
@@ -1,6 +1,8 @@
 package com.zy.spring.ioc;
 
+import com.zy.spring.ioc.controller.UserController;
 import com.zy.spring.ioc.dao.UserDao;
+import com.zy.spring.ioc.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,26 @@ public class Config {
     @Bean  //java Config利用方法创建对象，将方法返回的对象放入容器，beanId=方法名
     public UserDao userDao() {
         UserDao userDao = new UserDao();
+        System.out.println("已创建"+userDao);
         return userDao;
+    }
+
+    //java config对象依赖注入
+    @Bean //相当于 <bean id="XXX" class="xxx">
+    public UserService userService(UserDao userDao ){
+        UserService userService = new UserService();
+        System.out.println("已创建"+userService);
+        userService.setUserDao(userDao);
+        System.out.println("调用setUserDao:"+userDao);
+        return userService;
+    }
+
+    @Bean
+    public UserController userController(UserService userService){
+        UserController userController = new UserController();
+        System.out.println("已创建"+userController);
+        userController.setUserService(userService);
+        System.out.println("调用setUserService:"+userService);
+        return userController;
     }
 }
--- src/main/java/com/zy/spring/ioc/SpringApplication.java
+++ src/main/java/com/zy/spring/ioc/SpringApplication.java
@@ -8,7 +8,7 @@ public class SpringApplication {
     public static void main(String[] args) {
         //基于Java Config配置IoC容器初始化
         ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-
+        System.out.println("==============================");
         //获取beanId
         String[] ids = context.getBeanDefinitionNames();
         for (String id : ids) {
