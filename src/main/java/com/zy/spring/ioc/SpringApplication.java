2902cfa510c6082be96023512f313c65f33cff20
zy <1661996964@qq.com>
2022-11-07 22:22:10 +0800

    基于Java Config配置IoC容器初始化，获取beanId并打印

---
+++ src/main/java/com/zy/spring/ioc/SpringApplication.java
@@ -0,0 +1,18 @@
+package com.zy.spring.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringApplication {
+    public static void main(String[] args) {
+        //基于Java Config配置IoC容器初始化
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+
+        //获取beanId
+        String[] ids = context.getBeanDefinitionNames();
+        for (String id : ids) {
+            System.out.println(id+":"+context.getBean(id));
+        }
+    }
+}
